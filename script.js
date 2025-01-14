5ce05a71c21b35c90568dcc89270cbd3a8a3fe3e
144Jahnavi <114974749+144Jahnavi@users.noreply.github.com>
2025-01-14 18:03:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('collapse-btn').addEventListener('click', () => {
+    const menu = document.getElementById('menu-items');
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+  });
+  
